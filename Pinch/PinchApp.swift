c065749eb46152aa6c4df57f2dbb7f275fe77097
Resn4R <purview-joust.0x@icloud.com>
2024-01-01 12:30:56 +0100

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Vito Borghi on 01/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
