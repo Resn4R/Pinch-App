9a1a5d7bf4e25f59356d39a0b273ca2ca875a390
Resn4R <purview-joust.0x@icloud.com>
2024-01-03 18:24:20 +0000

    implemented drag gesture, updated tap gesture to reset offset

--- Pinch/Screens/ContentView.swift
+++ Pinch/Screens/ContentView.swift
@@ -10,6 +10,15 @@ import SwiftUI
 struct ContentView: View {
     @State private var isAnimating = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
+    
+    func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
+    
     var body: some View {
         NavigationStack{
             ZStack{
@@ -20,7 +29,8 @@ struct ContentView: View {
                     .clipShape(RoundedRectangle(cornerRadius: 10))
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
-                    .opacity(isAnimating ? 1 : 0) 
+                    .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
                 //MARK: TAP GESTURE
                     .onTapGesture(count: 2) {
@@ -29,11 +39,23 @@ struct ContentView: View {
                                 imageScale = 5
                             }
                         } else {
-                            withAnimation(.spring()){
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
-                    }
+                    } //:TAP GESTURE
+                //MARK: DRAG GESTURE
+                    .gesture (
+                        DragGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            }
+                            .onEnded({ _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            })
+                    )
             }//:ZSTACK
             .onAppear {
                 withAnimation(.linear(duration: 1)){
